8c61fc2068e2f272adbe3bb5d10fe15840fec7ab
John Wood <j@jdtw.us>
2018-04-18 00:11:51 -0700

    Some NCRYPT is working

--- src/lib.rs
+++ src/lib.rs
@@ -10,13 +10,18 @@ use std::collections::HashMap;
 
 extern crate winapi;
 
-mod ncrypt {
+pub mod ncrypt {
     use winapi::shared::bcrypt::*;
-    use winapi::shared::minwindef::{ULONG};
-    use winapi::um::winnt::{LPCWSTR};
+    use winapi::shared::minwindef::ULONG;
+    use winapi::um::winnt::LPCWSTR;
     use winapi::shared::basetsd::ULONG_PTR;
+    use std::ffi::OsStr;
+    use std::iter::once;
+    use std::os::windows::ffi::OsStrExt;
+    use std::ptr::null_mut;
 
     type DWORD = ULONG;
+    type SECURITY_STATUS = i32;
 
     const NCRYPT_SILENT_FLAG: ULONG = 0x00000040;
     const NCRYPT_IGNORE_DEVICE_STATE_FLAG: ULONG = 0x00001000;
@@ -28,29 +33,200 @@ mod ncrypt {
     type NCRYPT_SECRET_HANDLE = ULONG_PTR;
 
     const MS_KEY_STORAGE_PROVIDER: &'static str = "Microsoft Software Key Storage Provider";
-    const MS_SMART_CARD_KEY_STORAGE_PROVIDER: &'static str = "Microsoft Smart Card Key Storage Provider";
+    const MS_SMART_CARD_KEY_STORAGE_PROVIDER: &'static str =
+        "Microsoft Smart Card Key Storage Provider";
     const MS_PLATFORM_KEY_STORAGE_PROVIDER: &'static str = "Microsoft Platform Crypto Provider";
     const MS_NGC_KEY_STORAGE_PROVIDER: &'static str = "Microsoft Passport Key Storage Provider";
 
-    enum Ksp {
+    pub enum Ksp {
         Software,
         SmartCard,
         Tpm,
         Ngc,
     }
 
+    #[link(name = "ncrypt")]
     extern "stdcall" {
         fn NCryptOpenStorageProvider(
             phProvider: *mut NCRYPT_PROV_HANDLE,
             pszProviderName: LPCWSTR,
-            dwFlags: DWORD
-        ) -> NTSTATUS;
+            dwFlags: DWORD,
+        ) -> SECURITY_STATUS;
+
+        fn NCryptFreeObject(hObject: NCRYPT_HANDLE) -> SECURITY_STATUS;
+
+        fn NCryptCreatePersistedKey(
+            hProvider: NCRYPT_PROV_HANDLE,
+            phKey: *mut NCRYPT_KEY_HANDLE,
+            pszAlgId: LPCWSTR,
+            pszKeyName: LPCWSTR,
+            dwLegacyKeySpec: DWORD,
+            dwFlags: DWORD,
+        ) -> SECURITY_STATUS;
+
+        fn NCryptFinalizeKey(hKey: NCRYPT_KEY_HANDLE, dwFlags: DWORD) -> SECURITY_STATUS;
+
+        fn NCryptDeleteKey(hkey: NCRYPT_KEY_HANDLE, dwFlags: DWORD) -> SECURITY_STATUS;
+
+        fn NCryptExportKey(
+            hKey: NCRYPT_KEY_HANDLE,
+            hExportKey: NCRYPT_KEY_HANDLE,
+            pszBlobType: LPCWSTR,
+            pParameterList: *mut BCryptBufferDesc,
+            pOutput: *mut u8,
+            cbOutput: DWORD,
+            pcbResult: *mut DWORD,
+            dwFlags: DWORD,
+        ) -> SECURITY_STATUS;
     }
 
-    fn ncrypt_open_storage_provider(ksp: Ksp) -> Result<(), NTSTATUS> {
-        unsafe {
+    pub struct NCryptHandle {
+        handle: NCRYPT_HANDLE,
+    }
+
+    impl NCryptHandle {
+        fn new() -> NCryptHandle {
+            NCryptHandle { handle: 0 }
         }
-        panic!("notimpl");
+
+        fn release_and_get_addressof(&mut self) -> *mut NCRYPT_HANDLE {
+            self.reset();
+            &mut self.handle
+        }
+
+        fn get(&self) -> NCRYPT_HANDLE {
+            self.handle
+        }
+
+        fn reset(&mut self) {
+            unsafe {
+                if self.handle != 0 {
+                    NCryptFreeObject(self.handle);
+                    self.handle = 0;
+                }
+            }
+        }
+
+        fn release(&mut self) -> NCRYPT_HANDLE {
+            let tmp = self.handle;
+            self.handle = 0;
+            tmp
+        }
+    }
+
+    impl Drop for NCryptHandle {
+        fn drop(&mut self) {
+            self.reset();
+        }
+    }
+
+    fn lpcwstr(string: &str) -> Vec<u16> {
+        OsStr::new(string).encode_wide().chain(once(0)).collect()
+    }
+
+    pub fn open_storage_provider(ksp: Ksp) -> Result<NCryptHandle, SECURITY_STATUS> {
+        let prov_name = match ksp {
+            Ksp::Software => MS_KEY_STORAGE_PROVIDER,
+            Ksp::SmartCard => MS_SMART_CARD_KEY_STORAGE_PROVIDER,
+            Ksp::Tpm => MS_PLATFORM_CRYPTO_PROVIDER,
+            Ksp::Ngc => MS_NGC_KEY_STORAGE_PROVIDER,
+        };
+        let mut prov = NCryptHandle::new();
+        let status = unsafe {
+            NCryptOpenStorageProvider(
+                prov.release_and_get_addressof(),
+                lpcwstr(prov_name).as_ptr(),
+                0,
+            )
+        };
+        if status != 0 {
+            Err(status)
+        } else {
+            Ok(prov)
+        }
+    }
+
+    pub enum Algorithm {
+        EcdhP256,
+    }
+
+    pub fn create_persisted_key(
+        provider: &NCryptHandle,
+        algo: Algorithm,
+        key_name: &str, // TODO: Option<&str> for ephemeral keys
+    ) -> Result<NCryptHandle, SECURITY_STATUS> {
+        let algorithm = match algo {
+            Algorithm::EcdhP256 => BCRYPT_ECDH_P256_ALGORITHM,
+        };
+        let mut key = NCryptHandle::new();
+        let status = unsafe {
+            NCryptCreatePersistedKey(
+                provider.get(),
+                key.release_and_get_addressof(),
+                lpcwstr(algorithm).as_ptr(),
+                lpcwstr(key_name).as_ptr(),
+                0,
+                0,
+            )
+        };
+        if status != 0 {
+            return Err(status);
+        }
+        Ok(key)
+    }
+
+    pub fn finalize_key(key: &NCryptHandle) -> Result<(), SECURITY_STATUS> {
+        let status = unsafe { NCryptFinalizeKey(key.get(), 0) };
+        if status != 0 {
+            return Err(status);
+        }
+        Ok(())
+    }
+
+    pub fn delete_key(mut key: NCryptHandle) -> Result<(), SECURITY_STATUS> {
+        let status = unsafe { NCryptDeleteKey(key.release(), 0) };
+        if status != 0 {
+            return Err(status);
+        }
+        Ok(())
+    }
+
+    pub fn export_key(key: &NCryptHandle) -> Result<Vec<u8>, SECURITY_STATUS> {
+        unsafe {
+            let mut byte_count: u32 = 0;
+            let status = NCryptExportKey(
+                key.get(),
+                0,
+                lpcwstr(BCRYPT_ECCPUBLIC_BLOB).as_ptr(),
+                null_mut(),
+                null_mut(),
+                0,
+                &mut byte_count,
+                0,
+            );
+            if status != 0 {
+                return Err(status);
+            }
+
+            let mut output = Vec::with_capacity(byte_count as usize);
+            let status = NCryptExportKey(
+                key.get(),
+                0,
+                // TODO: Create an enum
+                lpcwstr(BCRYPT_ECCPUBLIC_BLOB).as_ptr(),
+                null_mut(),
+                output.as_mut_ptr(),
+                byte_count,
+                &mut byte_count,
+                0,
+            );
+            if status != 0 {
+                return Err(status);
+            }
+
+            output.set_len(byte_count as usize);
+            Ok(output)
+        }
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
 extern crate pwrs;
 
+use pwrs::ncrypt;
+use pwrs::ncrypt::{Algorithm, Ksp};
+
 fn main() {
+    let prov = ncrypt::open_storage_provider(Ksp::Software).unwrap();
+    let key = ncrypt::create_persisted_key(&prov, Algorithm::EcdhP256, "foobarbazquux").unwrap();
+    ncrypt::finalize_key(&key).unwrap();
+    let pubkey = ncrypt::export_key(&key).unwrap();
+    println!("pub: {} {:?}", pubkey.len(), pubkey);
+    ncrypt::delete_key(key).unwrap();
 }
