1bac1309d2fc3726d5298a692092ed1f431b00ac
John Wood <j@jdtw.us>
2018-05-05 20:36:42 -0700

    Refactor huge match

--- src/main.rs
+++ src/main.rs
@@ -121,7 +121,7 @@ fn main() {
     }
 }
 
-fn vault_path_from_matches(matches: &ArgMatches) -> Result<PathBuf, Error> {
+fn vault_path(matches: &ArgMatches) -> Result<PathBuf, Error> {
     Ok(PathBuf::from(matches
         .value_of("vault")
         .map_or_else(|| std::env::var("VAULT_PATH"), |s| Ok(String::from(s)))
@@ -130,104 +130,121 @@ fn vault_path_from_matches(matches: &ArgMatches) -> Result<PathBuf, Error> {
         )?))
 }
 
-fn run(matches: ArgMatches) -> Result<(), Error> {
-    let vault_path = vault_path_from_matches(&matches)?;
-    match matches.subcommand() {
-        ("new", Some(new_matches)) => {
-            let ksp = if new_matches.is_present("software") {
-                Ksp::Software
-            } else if new_matches.is_present("smartcard") {
-                Ksp::SmartCard
-            } else {
-                unreachable!()
-            };
-            let key_name = String::from(new_matches.value_of("key").unwrap());
-            let authenticator = KeyStorageProvider::new(ksp, key_name)?;
-            let vault = Vault::new(authenticator);
-            let vault_file = OpenOptions::new()
-                .write(true)
-                .create_new(true)
-                .open(vault_path.as_path())
-                .context(format!(
-                    "Create new vault file failed: {}",
-                    vault_path.display()
-                ))?;
-            vault
-                .to_writer(vault_file)
-                .context("Serialize vault failed")?;
-        }
-        ("add", Some(add_matches)) => {
-            let site = String::from(add_matches.value_of("SITE").unwrap());
-            let (username, password) = if add_matches.is_present("user") {
-                (
-                    String::from(add_matches.value_of("user").unwrap()),
-                    String::from(add_matches.value_of("password").unwrap()),
-                )
-            } else {
-                let message = format!("Enter credentials for {}", site);
-                UIPrompt::new("PWRS", &message)
-                    .prompt()
-                    .context(format!("Prompt '{}' cancelled", message))?
-                    .to_tuple()
-            };
+fn new(vault_path: PathBuf, matches: &ArgMatches) -> Result<(), Error> {
+    let ksp = if matches.is_present("software") {
+        Ksp::Software
+    } else if matches.is_present("smartcard") {
+        Ksp::SmartCard
+    } else {
+        unreachable!()
+    };
+    let key_name = String::from(matches.value_of("key").unwrap());
+    let authenticator = KeyStorageProvider::new(ksp, key_name)?;
+    let vault = Vault::new(authenticator);
+    let vault_file = OpenOptions::new()
+        .write(true)
+        .create_new(true)
+        .open(vault_path.as_path())
+        .context(format!(
+            "Create new vault file failed: {}",
+            vault_path.display()
+        ))?;
+    vault
+        .to_writer(vault_file)
+        .context("Serialize vault failed")?;
+    Ok(())
+}
 
-            let vault_file = File::open(vault_path.as_path())
-                .context(format!("Open vault file failed: {}", vault_path.display()))?;
-            let mut vault = Vault::from_reader(vault_file)?;
-            vault
-                .insert(site, username, &password)
-                .context("Insert vault entry failed")?;
-            let vault_file = OpenOptions::new()
-                .write(true)
-                .truncate(true)
-                .open(vault_path.as_path())
-                .context(format!(
-                    "Open vault file for write failed: {}",
-                    vault_path.display()
-                ))?;
-            vault.to_writer(vault_file)?;
-        }
-        ("get", Some(get_matches)) => {
-            let site = get_matches.value_of("SITE").unwrap();
-            let vault_file = File::open(vault_path.as_path())
-                .context(format!("Open vault file failed: {}", vault_path.display()))?;
-            let vault = Vault::from_reader(vault_file)?;
-            if let Some(entry) = vault.get(site) {
-                println!("Username: {}", entry.username());
-                if !get_matches.is_present("user") {
-                    let password = entry
-                        .decrypt_password()
-                        .context("Password decryption failed")?;
-                    if get_matches.is_present("dump") {
-                        println!("Password: {}", password);
-                    } else {
-                        Clipboard::new()?.set_string(&password)?;
-                        println!("Password copied to clipboard");
-                    }
-                }
+fn add(vault_path: PathBuf, matches: &ArgMatches) -> Result<(), Error> {
+    let site = String::from(matches.value_of("SITE").unwrap());
+    let (username, password) = if matches.is_present("user") {
+        (
+            String::from(matches.value_of("user").unwrap()),
+            String::from(matches.value_of("password").unwrap()),
+        )
+    } else {
+        let message = format!("Enter credentials for {}", site);
+        UIPrompt::new("PWRS", &message)
+            .prompt()
+            .context(format!("Prompt '{}' cancelled", message))?
+            .to_tuple()
+    };
+
+    let vault_file = File::open(vault_path.as_path())
+        .context(format!("Open vault file failed: {}", vault_path.display()))?;
+    let mut vault = Vault::from_reader(vault_file)?;
+    vault
+        .insert(site, username, &password)
+        .context("Insert vault entry failed")?;
+    let vault_file = OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .open(vault_path.as_path())
+        .context(format!(
+            "Open vault file for write failed: {}",
+            vault_path.display()
+        ))?;
+    vault.to_writer(vault_file)?;
+
+    Ok(())
+}
+
+fn get(vault_path: PathBuf, matches: &ArgMatches) -> Result<(), Error> {
+    let site = matches.value_of("SITE").unwrap();
+    let vault_file = File::open(vault_path.as_path())
+        .context(format!("Open vault file failed: {}", vault_path.display()))?;
+    let vault = Vault::from_reader(vault_file)?;
+    if let Some(entry) = vault.get(site) {
+        println!("Username: {}", entry.username());
+        if !matches.is_present("user") {
+            let password = entry
+                .decrypt_password()
+                .context("Password decryption failed")?;
+            if matches.is_present("dump") {
+                println!("Password: {}", password);
+            } else {
+                Clipboard::new()?.set_string(&password)?;
+                println!("Password copied to clipboard");
             }
         }
-        ("ls", Some(_ls_matches)) => {
-            let vault_file = File::open(vault_path.as_path())
-                .context(format!("Open vault file failed: {}", vault_path.display()))?;
-            let vault = Vault::from_reader(vault_file)?;
-            // TODO: Sort these by site
-            for (site, entry) in vault.iter() {
-                println!("{}, {}", site, entry.username());
-            }
-        }
-        ("del", Some(_del_matches)) => {
-            let vault_file = File::open(vault_path.as_path())
-                .context(format!("Open vault file failed: {}", vault_path.display()))?;
-            let vault = Vault::from_reader(vault_file)?;
-            vault.delete().context("Failed to remove the vault key")?;
-            fs::remove_file(vault_path.as_path()).context(format!(
-                "Remove vault file failed: {}",
-                vault_path.display()
-            ))?;
-        }
-        _ => unreachable!(),
     }
 
     Ok(())
+}
+
+fn ls(vault_path: PathBuf) -> Result<(), Error> {
+    let vault_file = File::open(vault_path.as_path())
+        .context(format!("Open vault file failed: {}", vault_path.display()))?;
+    let vault = Vault::from_reader(vault_file)?;
+    // TODO: Sort these by site
+    for (site, entry) in vault.iter() {
+        println!("{}, {}", site, entry.username());
+    }
+
+    Ok(())
+}
+
+fn del(vault_path: PathBuf) -> Result<(), Error> {
+    let vault_file = File::open(vault_path.as_path())
+        .context(format!("Open vault file failed: {}", vault_path.display()))?;
+    let vault = Vault::from_reader(vault_file)?;
+    vault.delete().context("Failed to remove the vault key")?;
+    fs::remove_file(vault_path.as_path()).context(format!(
+        "Remove vault file failed: {}",
+        vault_path.display()
+    ))?;
+
+    Ok(())
+}
+
+fn run(matches: ArgMatches) -> Result<(), Error> {
+    let vault_path = vault_path(&matches)?;
+    match matches.subcommand() {
+        ("new", Some(matches)) => new(vault_path, matches),
+        ("add", Some(matches)) => add(vault_path, matches),
+        ("get", Some(matches)) => get(vault_path, matches),
+        ("ls", _) => ls(vault_path),
+        ("del", _) => del(vault_path),
+        _ => unreachable!(),
+    }
 }
